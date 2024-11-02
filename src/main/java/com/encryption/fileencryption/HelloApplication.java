2625f86e448e96a6be061b1bb9e99872cfffe20d
surya <mohandasssurya22@gmail.com>
2024-11-02 22:08:09 +0530

    Added UI

--- src/main/java/com/encryption/fileencryption/HelloApplication.java
+++ src/main/java/com/encryption/fileencryption/HelloApplication.java
@@ -5,23 +5,49 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
     @FXML
     Button buttonChooseFile, buttonProcess;
+    @FXML
+    Label labelFileLocation;
+    @FXML
+    RadioButton radioButtonEncrypt, radioButtonDecrypt;
+
+    String path = "";
 
     public void chooseFile(ActionEvent actionEvent) {
-        System.out.println("Pressed");
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(null);
+        path = file.getAbsolutePath();
+        labelFileLocation.setText(path);
     }
 
     public void process(ActionEvent actionEvent) {
-        System.out.println("Processed");
+        if(path != "") {
+            if (radioButtonDecrypt.isSelected()) {
+                System.out.println("1");
+            }
+            if (radioButtonEncrypt.isSelected()) {
+                System.out.println("2");
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid");
+            alert.setHeaderText(null); // Optional, you can remove this line if you want a header
+            alert.setContentText("Please select a file to encrypt/decrypt");
+            alert.showAndWait();
+        }
     }
 
     @Override
@@ -30,11 +56,6 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-
-        FileChooser fileChooser = new FileChooser();
-
-
-
 
         stage.show();
 
